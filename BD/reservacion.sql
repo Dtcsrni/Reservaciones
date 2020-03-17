67fc3d12289de9e7249e65cd98e25472395890d4
Dtcsrni <evega-12@hotmail.com>
2020-03-17 08:51:04 -0600

    -Cambios en nombres y estructura de tablas

--- BD/reservacion.sql
+++ BD/reservacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-03-2020 a las 15:53:21
+-- Tiempo de generación: 17-03-2020 a las 15:50:19
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.2
 
@@ -42,7 +42,8 @@ CREATE TABLE `espacios` (
 INSERT INTO `espacios` (`id_espacio`, `nombre_espacio`, `tipo_espacio`, `lugares`) VALUES
 (9, 'Aula 70', 'Laboratorio de Computo', 40),
 (10, 'Aula 40', 'Laboratorio de Computo', 40),
-(14, 'Cafetería', 'Sala familiar', 100);
+(14, 'Cafetería', 'Sala familiar', 100),
+(15, 'Aul 70', 'Laboratorio de Computo', 50);
 
 -- --------------------------------------------------------
 
@@ -52,12 +53,22 @@ INSERT INTO `espacios` (`id_espacio`, `nombre_espacio`, `tipo_espacio`, `lugares
 
 CREATE TABLE `reserva` (
   `id_reserva` int(11) NOT NULL,
-  `id_espacio` int(11) NOT NULL,
+  `nombre_espacio` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
   `horario` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
   `fecha` date NOT NULL,
-  `id_usuario` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
-  `lugares` int(11) NOT NULL
+  `nombre_usuario` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `lugares_disponibles` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `reserva`
+--
+
+INSERT INTO `reserva` (`id_reserva`, `nombre_espacio`, `horario`, `fecha`, `nombre_usuario`, `lugares_disponibles`) VALUES
+(1, 'Aula 70', '7:00-9:00', '2020-03-19', 'Erick Vega', 40),
+(2, 'Aula 70', '9:00-11:00', '2020-03-19', 'Erick Vega', 40),
+(3, 'Aula 70', '18:00-20:00', '2020-03-19', 'Erick Vega', 45),
+(6, 'Cafetería', '18:00-20:00', '2020-04-02', 'Monica Manchuco', 40);
 
 -- --------------------------------------------------------
 
@@ -78,7 +89,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `tipo`, `contra`, `grupo`) VALUES
-(1, 'Erick Renato Vega Cerón', 'Administrador', '1', '17');
+(1, 'Erick Renato Vega Cerón', 'Administrador', '1', '17'),
+(123, 'Marlon Brandon', 'Usuario', '123456', ''),
+(123456, 'Monica Manchuco', 'Usuario', '123456', '');
 
 --
 -- Índices para tablas volcadas
@@ -111,13 +124,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `espacios`
 --
 ALTER TABLE `espacios`
-  MODIFY `id_espacio` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_espacio` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
